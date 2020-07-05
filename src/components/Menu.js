f1727c90aebb7417e9e4a3fec0ed19a342a92c14
julien <git@julienc.io>
2020-07-05 17:23:20 +0200

    fix url errors on github pages

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-import pagesMapping from '../views'
+import pagesMapping, { homepageUrl } from '../views'
 import logo from '../assets/images/logo.png'
 import './Menu.scss'
 
@@ -9,11 +9,11 @@ const Menu = () => {
   const { t } = useTranslation()
   return (
     <nav>
-      <NavLink className='link-home' to='/' exact>
+      <NavLink className='link-home' to={homepageUrl} exact>
         <img src={logo} alt={t('Menu.home')} />
       </NavLink>
       <ul className='links'>
-        {Object.entries(pagesMapping).filter(([path, _]) => path !== '/').map(([path, Component]) => (
+        {Object.entries(pagesMapping).filter(([path, _]) => path !== homepageUrl).map(([path, Component]) => (
           <li className='link-item' key={path}>
             <NavLink className='link' to={path} exact>{t(`Menu.${Component.name.toLowerCase()}`)}</NavLink>
           </li>
--- src/views/index.js
+++ src/views/index.js
@@ -5,12 +5,14 @@ import Discography from './Discography'
 import Home from './Home'
 import './index.scss'
 
+export const homepageUrl = `${process.env.PUBLIC_URL}/`
+
 const pagesMapping = {
-  '/': Home,
-  '/biographie': Biography,
-  '/discographie': Discography,
-  '/publicites': Advertisments,
-  '/contact': Contact
+  [homepageUrl]: Home,
+  [`${process.env.PUBLIC_URL}/biographie`]: Biography,
+  [`${process.env.PUBLIC_URL}/discographie`]: Discography,
+  [`${process.env.PUBLIC_URL}/publicites`]: Advertisments,
+  [`${process.env.PUBLIC_URL}/contact`]: Contact
 }
 
 export default pagesMapping
